983c2f95d32ade94dcc8f11251548187af3f4567
Lalu Iman Abdullah <macbookm1@Lalu-Iman-Abdullah.local>
2023-09-29 13:57:59 +0700

    third commit

--- js.js
+++ js.js
@@ -7,9 +7,10 @@ ScrollReveal({
     delay:200
  });
 
- ScrollReveal().reveal('.home-img,.home-img2, .home-video, .macbook-img, .air-img img' , { origin: 'bottom'});
- ScrollReveal().reveal('.home-text h1, .ipad-img, .ipad2-img, .air-img2 .left' , { origin: 'right'});
- ScrollReveal().reveal('.m2-img, .air-img2 .right', { origin: 'left'});
+ ScrollReveal().reveal('.home-img,.home-img2, .home-video, .macbook-img, .air-img img, .ipad-img' , { origin: 'bottom'});
+ ScrollReveal().reveal('.home-text h1' , { origin: 'right'});
+ ScrollReveal().reveal('.m2-img, .air-img2 .right, .ipad2-img', { origin: 'left'});
+ ScrollReveal().reveal('.air-img2 .left', { origin: 'top'});
 //  toggle
  let menu =document.querySelector("#menu-icon");
  let navbar=document.querySelector(".navbar");
